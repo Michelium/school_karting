1398d5865f0ed21fd3c0cc4951ab7605f3bf5e0a
Michelium <michelhamelink@gmail.com>
2020-03-17 22:14:59 +0100

    ticket a7

--- src/Controller/DeelnemerController.php
+++ src/Controller/DeelnemerController.php
@@ -3,48 +3,56 @@
 namespace App\Controller;
 
 
+use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-class DeelnemerController extends AbstractController
-{
+class DeelnemerController extends AbstractController {
+
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder) {
+        $this->encoder = $encoder;
+    }
+
     /**
      * @Route("/user/activiteiten", name="activiteiten")
      */
-    public function activiteitenAction()
-    {
-        $usr= $this->get('security.token_storage')->getToken()->getUser();
+    public function activiteitenAction() {
+        $usr = $this->get('security.token_storage')->getToken()->getUser();
 
-        $beschikbareActiviteiten=$this->getDoctrine()
+        $beschikbareActiviteiten = $this->getDoctrine()
             ->getRepository('App:Activiteit')
-        ->getBeschikbareActiviteiten($usr->getId());
+            ->getBeschikbareActiviteiten($usr->getId());
 
-        $ingeschrevenActiviteiten=$this->getDoctrine()
+        $ingeschrevenActiviteiten = $this->getDoctrine()
             ->getRepository('App:Activiteit')
             ->getIngeschrevenActiviteiten($usr->getId());
 
-        $totaal=$this->getDoctrine()
+        $totaal = $this->getDoctrine()
             ->getRepository('App:Activiteit')
             ->getTotaal($ingeschrevenActiviteiten);
 
 
         return $this->render('deelnemer/activiteiten.html.twig', [
-                'beschikbare_activiteiten'=>$beschikbareActiviteiten,
-                'ingeschreven_activiteiten'=>$ingeschrevenActiviteiten,
-                'totaal'=>$totaal,
+            'beschikbare_activiteiten' => $beschikbareActiviteiten,
+            'ingeschreven_activiteiten' => $ingeschrevenActiviteiten,
+            'totaal' => $totaal,
         ]);
     }
 
     /**
      * @Route("/user/inschrijven/{id}", name="inschrijven")
      */
-    public function inschrijvenActiviteitAction($id)
-    {
+    public function inschrijvenActiviteitAction($id) {
 
         $activiteit = $this->getDoctrine()
             ->getRepository('App:Activiteit')
             ->find($id);
-        $usr= $this->get('security.token_storage')->getToken()->getUser();
+        $usr = $this->get('security.token_storage')->getToken()->getUser();
         $usr->addActiviteit($activiteit);
 
         $em = $this->getDoctrine()->getManager();
@@ -57,17 +65,47 @@ class DeelnemerController extends AbstractController
     /**
      * @Route("/user/uitschrijven/{id}", name="uitschrijven")
      */
-    public function uitschrijvenActiviteitAction($id)
-    {
+    public function uitschrijvenActiviteitAction($id) {
         $activiteit = $this->getDoctrine()
             ->getRepository('App:Activiteit')
             ->find($id);
-        $usr= $this->get('security.token_storage')->getToken()->getUser();
+        $usr = $this->get('security.token_storage')->getToken()->getUser();
         $usr->removeActiviteit($activiteit);
         $em = $this->getDoctrine()->getManager();
         $em->persist($usr);
         $em->flush();
         return $this->redirectToRoute('activiteiten');
+    }
+
+    /**
+     * @Route("/user/profile/", name="deelnemer_profile")
+     */
+    public function profile(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+        $userProvider = $this->getUser();
+        $user = $em->getRepository(User::class)->findOneBy(['username' => $userProvider->getUsername()]);
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            if (isset($request->request->all()['user']['plainPassword'])) {
+                $plainPassword = $request->request->all()['user']['plainPassword']['first'];
+                $encodedPassword = $this->encoder->encodePassword($user, $plainPassword);
+
+                $user->setPassword($encodedPassword);
+            }
+
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('notice', 'Profiel succesvol aangepast');
+            return $this->redirectToRoute('deelnemer_profile');
+        }
+
+        return $this->render('deelnemer/profile.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
 }
